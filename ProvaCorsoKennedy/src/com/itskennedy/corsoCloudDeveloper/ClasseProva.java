d79046143c06cccb9aa454863cad6fb9db6b2f3d
ITSSpagnolG <ITSSpagnolG@itscor207233.itsacademy.corvallisholding.it>
2021-03-03 10:20:43 +0100

    ciao1

--- ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
+++ ProvaCorsoKennedy/src/com/itskennedy/corsoCloudDeveloper/ClasseProva.java
@@ -5,6 +5,7 @@ public class ClasseProva {
 	public static void main(String[] args) {
 		System.out.println("Ciao a tutti");
 		System.out.println("ciao");
+		System.out.println("" + "");
 		System.out.println("ciao ancora");
 	}
 	
